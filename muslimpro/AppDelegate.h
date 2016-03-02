05c77c59f947c10f1003ba61362fe11908b84f20
Student 1 <student1@josephs-macbook-air.local>
2016-03-02 16:15:30 -0500

    Initial Commit

---
+++ muslimpro/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  muslimpro
+//
+//  Created by Student 1 on 3/2/16.
+//  Copyright © 2016 Student 1. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ muslimpro/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  muslimpro
+//
+//  Created by Student 1 on 3/2/16.
+//  Copyright © 2016 Student 1. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
